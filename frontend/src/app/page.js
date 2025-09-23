80644fc46c06087aa703b2ebd34eb63ec341ebf8
TraderLfc <lefausa1169@gmail.com>
2025-09-22 23:13:11 -0400

    update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="bg-red flex justify-center text-sm">Holaa</div>
+      <div className="bg-red flex justify-center text-sm">Hola Mundo</div>
     </div>
   );
 }
